467aea3af76e90cc30d17543b88ff8683d4c6c7e
Rahul Kumar <rahul.tanti.software@gmail.com>
2026-05-14 22:51:38 +0530

    first commit

--- src/main/java/com/form/device/gateway/service/DeviceWebSocketHandler.java
+++ src/main/java/com/form/device/gateway/service/DeviceWebSocketHandler.java
@@ -2,12 +2,14 @@ package com.form.device.gateway.service;
 import com.form.device.gateway.dto.DeviceConnection;
 import com.form.device.gateway.security.AuthTokenValidator;
 import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.socket.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public record DeviceWebSocketHandler(
         DeviceSessionManager sessionManager,
         AuthTokenValidator validator,
@@ -20,10 +22,12 @@ public record DeviceWebSocketHandler(
                 .build()
                 .getQueryParams();
         String token = queryParams.getFirst("token");
-
+        log.debug("connection started .............");
         if (token == null || token.isEmpty()) {
+            log.debug("token param is null");
             return session.close();
         }
+
 
         DeviceConnection connection = new DeviceConnection(session);
         String deviceCode;
@@ -35,18 +39,21 @@ public record DeviceWebSocketHandler(
             String redisValue         = redis.opsForValue().get("token_version:" + userId);
 
             if (redisValue == null) {
+                log.debug("tokenVersion null");
                 return session.close();
             }
 
             int tokenVersionFromDb = Integer.parseInt(redisValue);
 
             if (tokenVersionFromToken != tokenVersionFromDb) {
+                log.debug("tokenVersion mismatch");
                 return session.close();
             }
 
             sessionManager.register(deviceCode, connection);
 
         } catch (Exception e) {
+            log.debug("connection failed :: "+e.getMessage());
             return session.close();
         }
 

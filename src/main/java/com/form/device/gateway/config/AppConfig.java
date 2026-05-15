2558a6741c282097b257e747fc9a690eb37dc797
Rahul Kumar <rahul.tanti.software@gmail.com>
2026-05-15 09:02:21 +0530

    -- added subprotocol

--- src/main/java/com/form/device/gateway/config/AppConfig.java
+++ src/main/java/com/form/device/gateway/config/AppConfig.java
@@ -6,16 +6,24 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.form.device.gateway.security.AuthTokenValidator;
 import com.form.device.gateway.service.DeviceSessionManager;
 import com.form.device.gateway.service.DeviceWebSocketHandler;
+import io.micrometer.common.lang.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.HandshakeInfo;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.reactive.socket.server.support.*;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 @Configuration
 @EnableWebFlux
@@ -34,16 +42,40 @@ public class AppConfig {
     }
 
     @Bean
-    public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
-    }
-
-    @Bean
     public ObjectMapper redisObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
+
+    @Bean
+    public WebSocketService webSocketService() {
+        return new HandshakeWebSocketService(new ReactorNettyRequestUpgradeStrategy() {
+            @Override
+            public Mono<Void> upgrade(ServerWebExchange exchange,
+                                      WebSocketHandler handler,
+                                      @Nullable String subProtocol,
+                                      Supplier<HandshakeInfo> handshakeInfoFactory) {
+
+                List<String> protocols = exchange.getRequest()
+                        .getHeaders()
+                        .get("Sec-WebSocket-Protocol");
+
+                String protocol = (protocols != null && !protocols.isEmpty())
+                        ? protocols.get(0)
+                        : null;
+
+                return super.upgrade(exchange, handler, protocol, handshakeInfoFactory);
+            }
+        });
+    }
+
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter(WebSocketService webSocketService) {
+        return new WebSocketHandlerAdapter(webSocketService);
+    }
+
 
 }
